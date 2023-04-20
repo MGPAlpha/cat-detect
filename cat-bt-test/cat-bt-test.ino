2f1edea88f6d44518e87ad75b12169fde7ae8b42
Mitchell Philipp <mgpalpha1@gmail.com>
2023-04-20 01:24:44 -0400

    complete testing for hc06

--- cat-bt-test/cat-bt-test.ino
+++ cat-bt-test/cat-bt-test.ino
@@ -1,13 +1,18 @@
 #include <SoftwareSerial.h>
 
-SoftwareSerial Bluetooth(4, 3);
+SoftwareSerial Bluetooth(3, 2, false);
+
+#define BAUD_FACTOR 0
 
 void setup() {
   Serial.begin(9600);
-  Bluetooth.begin(38400);
+  Bluetooth.begin(9600 << BAUD_FACTOR);
 }
 
+int n = 0;
+
 void loop() {
+
     // Read from HC-05 and send data to the Arduino Serial Monitor
   if (Bluetooth.available())
     Serial.write(Bluetooth.read());
@@ -15,7 +20,12 @@ void loop() {
     // Read from Arduino Serial Monitor and send it to the HC-05
   if (Serial.available()) {
     int val = Serial.read();
-    Serial.write(val);
+    // Serial.write(val);
     Bluetooth.write(val); 
   }
+
+  // Bluetooth.println(n);
+  // Serial.println(n);
+  // delay(500);
+  // n++;
 }
